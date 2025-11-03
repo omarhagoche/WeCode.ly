819499f5964eab98c210d1dcf41ba9981fbdbb6c
omarhagoche <omar.hagoche@gmail.com>
2025-11-03 02:56:19 +0200

    back to github pages domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://wecode.ly/",
-  base: "/",
+  site: "https://omarhagoche.github.io/WeCode.ly/",
+  base: "/WeCode.ly/",
   integrations: [tailwind()],
   vite: {
     ssr: {
